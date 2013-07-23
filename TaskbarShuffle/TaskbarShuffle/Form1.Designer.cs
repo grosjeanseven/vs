97273af9b65cad4b072938e048529dff4d6e10b5
Grosjean Seven <mobile.grosjean@gmail.com>
2013-07-23 23:41:15 +0900

    first commit

---
+++ TaskbarShuffle/TaskbarShuffle/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ウィンドウ列挙";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "末尾に移動";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showToolStripMenuItem.Text = "表示(&V)";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "終了(&X)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 166);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.Name = "Form1";
+            this.Text = "Taskbar Shuffle";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TaskbarShuffle/TaskbarShuffle/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        // コールバックメソッドのデリゲート
+        private delegate int EnumerateWindowsCallback(IntPtr hWnd, int lParam);
+
+        // EnumWindows API関数の宣言
+        [DllImport("user32.dll", EntryPoint = "EnumWindows")]
+        private static extern int EnumWindows(EnumerateWindowsCallback lpEnumFunc, int lParam);
+
+        // GetWindowText API関数の宣言
+        [DllImport("user32.dll", EntryPoint = "GetWindowText", CharSet = CharSet.Auto)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCoun);
+
+        // IsWindowVisible API関数の宣言
+        [DllImport("user32.dll", EntryPoint = "IsWindowVisible")]
+        private static extern int IsWindowVisible(IntPtr hWnd);
+
+        /// <summary>
+        /// ウィンドウの表示状態を設定します。
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="nCmdShow"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "ShowWindow")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        // ウィンドウを列挙するためのコールバックメソッド
+        private int EnumerateWindows(IntPtr hWnd, int lParam)
+        {
+            const int BUFFER_SIZE = 0x1000;
+            StringBuilder sb = new StringBuilder(BUFFER_SIZE);
+
+            // ウィンドウが可視の場合
+            if (IsWindowVisible(hWnd) != 0)
+            {
+                // ウィンドウのキャプションを取得
+                if (GetWindowText(hWnd, sb, BUFFER_SIZE) != 0)
+                {
+                    DataRow row = _table.NewRow();
+                    row.SetField("ID", hWnd);
+                    row.SetField("NAME", sb);
+//                    row.ItemArray = new object[] { hWnd, sb };
+                    _table.Rows.Add(row);
+//                    Console.WriteLine(hWnd + " : " + sb);
+                }
+            }
+            // 列挙を継続するには0以外を返す必要がある
+            return 1;
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.notifyIcon1.Icon = Properties.Resources._1374604172_61522;
+            this.notifyIcon1.Text = this.Text;
+        }
+
+        private DataTable _table; 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _table = new DataTable();
+            _table.Columns.Add("ID", typeof(IntPtr));
+            _table.Columns.Add("NAME", typeof(string));
+            this.comboBox1.DataSource = _table;
+            this.comboBox1.DisplayMember = "NAME";
+            this.comboBox1.ValueMember = "ID";
+
+            // ウィンドウを列挙します。
+            EnumWindows(new EnumerateWindowsCallback(EnumerateWindows), 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IntPtr handle = new IntPtr( int.Parse(this.comboBox1.SelectedValue.ToString()));
+
+            ShowWindow(handle, SW_HIDE);
+            try
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    System.Threading.Thread.Sleep(10);
+                    Application.DoEvents();
+                }
+            }
+            finally
+            {
+                ShowWindow(handle, SW_SHOW);
+            }
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+    }
+}
